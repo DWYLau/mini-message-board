1edaf6b00cd2643b3adca8fbeb252ed06a03bd63
Derek <dereklau15@gmail.com>
2024-07-16 22:21:38 +0900

    Working post method, style create page

--- frontend/src/pages/CreateMessage.tsx
+++ frontend/src/pages/CreateMessage.tsx
@@ -1,7 +1,66 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { BsArrowLeft } from "react-icons/bs"
+import axios from "axios"
+
 const CreateMessage = () => {
+  const [message, setMessage] = useState("")
+  const [user, setUser] = useState("")
+  const navigate = useNavigate()
+
+  function postMessage() {
+    const data = { message, user }
+    axios
+      .post("http://localhost:5555/messages", data)
+      .then(() => {
+        navigate("/")
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
-    <div>
-      <h1>Create Message Page</h1>
+    <div className="flex flex-col justify-evenly items-center p-2 m-2 rounded-lg border-4 border-solid border-sky-800 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[500px] w-[600px]">
+      <div className="flex">
+        <Link
+          to={"/"}
+          className="absolute bottom-[26.5rem] right-[33rem] min-w-12 hover:border-red-700 hover:shadow-md p-2 border-2 border-red-500 rounded-xl text-white bg-red-500 transition ease-in-out delay-100"
+        >
+          <BsArrowLeft className="text-2xl" />
+        </Link>
+      </div>
+      <h1 className="font-extrabold text-3xl mx-5">Send a new message!</h1>
+      <div className="p-1 m-1 flex flex-col justify-center items-center gap-2">
+        <label htmlFor="user">Please enter your name.</label>
+        <input
+          className="border-solid border-2 rounded-md w-[300px]"
+          name="user"
+          id="user"
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+      </div>
+      <div className="p-1 m-1 flex flex-col justify-center items-center gap-2">
+        <label htmlFor="message">Please enter your message.</label>
+        <textarea
+          className="border-solid border-2 resize-none rounded-md h-[200px] w-[450px]"
+          name="message"
+          id="message"
+          placeholder="Enter your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          maxLength={100}
+        />
+      </div>
+
+      <button
+        onClick={postMessage}
+        className="min-w-20 hover:border-green-700 hover:shadow-md flex p-2 border-2 border-green-500 rounded-3xl text-white bg-green-500 transition ease-in-out delay-100"
+      >
+        Confirm
+      </button>
     </div>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { BsPlus } from "react-icons/bs"
 import DeleteForm from "../components/DeleteForm"
+import { Link } from "react-router-dom"
 
 const Home = () => {
   const [messages, setMessages] = useState<MessageType[]>([])
@@ -14,21 +15,8 @@ const Home = () => {
   const [deleteId, setDeleteId] = useState("")
 
   // function delete message - use Delete ID as part of URL
-  function deleteMessage() {
-    setLoading(true)
-    axios
-      .delete(`http://localhost:5555/messages/${deleteId}`)
-      .then(() => {
-        setShowForm(false)
-        setLoading(false)
-      })
-      .catch((error) => {
-        console.log(error)
-        setLoading(false)
-      })
-  }
 
-  useEffect(() => {
+  function getMessages() {
     setLoading(true)
     axios
       .get("http://localhost:5555/messages")
@@ -40,15 +28,36 @@ const Home = () => {
         console.log(error)
         setLoading(false)
       })
-  }, [showForm])
+  }
+
+  function deleteMessage() {
+    axios
+      .delete(`http://localhost:5555/messages/${deleteId}`)
+      .then(() => {
+        setShowForm(false)
+        getMessages()
+      })
+      .catch((error) => {
+        console.log(error)
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    getMessages()
+  }, [])
 
   return (
-    <div className=" rounded-lg border-4 border-solid border-sky-800 h-[830px] w-[1100px] overflow-y-scroll scrollbar scrollbar-thumb-sky-100 scrollbar-track-sky-300">
+    <div className="rounded-lg border-4 border-solid border-sky-800 h-[830px] w-[1100px] overflow-y-scroll scrollbar scrollbar-thumb-sky-100 scrollbar-track-sky-300">
       <header className="py-3 mx-10 m-2 flex justify-between items-center border-b-4 border-sky-800">
-        <button className="hover:border-green-700 hover:shadow-md flex p-2 border-2 border-green-500 rounded-3xl text-white bg-green-500 transition ease-in-out delay-100">
+        <Link
+          to={"/create"}
+          className="hover:border-green-700 hover:shadow-md flex p-2 border-2 border-green-500 rounded-3xl text-white bg-green-500 transition ease-in-out delay-100"
+        >
           <BsPlus className="text-2xl" />
           New Message
-        </button>
+        </Link>
         <h1 className="text-3xl font-bold mr-80">Mini Message Board</h1>
       </header>
       {loading ? (
